9afebef8f02f2b8521c7bc5a1c2a6077b9191621
Ilya Ryabchinski <ilyaryabchinski@gmail.com>
2018-09-13 19:58:49 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let res = 0;
+    const [div, pow] = divider(base);
+    while(number > 0){
+        number = Math.floor(number/div);
+        res += number;
+    }
+    return Math.floor(res/pow);
+}
+
+
+const divider = num => {
+    let result = 1;
+    let power = 1;
+    const finish = num
+    for(let i = 2; i < finish; i++){
+        if(num % i === 0){
+            let div = i;
+            let pow = 0;
+            while(num % i === 0){ 
+                pow++;
+                num /= i;
+        }
+        if(result ** power < div ** pow) {
+            result = div;
+            power = pow;
+        }
+        }
+        
+    }
+    return result === 1 ? [num, 1]: [result, power] ;
 }
